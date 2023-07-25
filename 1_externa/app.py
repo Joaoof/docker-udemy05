5e46745cd5bcc37574afc40cc9cbaed37660bbe2
joao <joaodeus400@gmail.com>
2023-07-25 15:47:38 -0300

    flask - python

--- 1_externa/app.py
+++ 1_externa/app.py
@@ -7,7 +7,7 @@ app.config["DEBUG"] = True
 
 @app.route("/", methods=["GET"])
 def index():
-    data = request.get('https://randomuser.me/api')
+    data = requests.get('https://randomuser.me/api')
     return data.json()
 
 if __name__ == "__main__":
---
+++ 3_conn_container/mysql/schema.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE flaskdoker;
+USE flaskdoker;
+
+CREATE TABLE `flaskdoker`. `users` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255),
+    PRIMARY KEY (ID));
